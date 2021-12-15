cfb893c4d9efcc0702440201ef215f3c1c8ab0d0
hanool <29415530+hanool@users.noreply.github.com>
2021-12-15 23:33:27 +0900

    🔧 changed canvas width & added css style moon phase

--- moon-phase.js
+++ moon-phase.js
@@ -1,11 +1,10 @@
 export class MoonPhase {
-  constructor(size) {
-    this.size = size;
-    this.canvas = document.createElement('canvas');
-    this.canvas.width = this.size;
-    this.canvas.height = this.size;
-    
-    this.ctx = this.canvas.getContext('2d');
+  constructor() {
+    this.canvas = document.getElementById("canvas");
+    this.canvas.width = document.body.clientWidth / 2;
+    this.canvas.height = document.body.clientWidth / 2;
+
+    this.ctx = this.canvas.getContext("2d");
 
     document.body.appendChild(this.canvas);
 
@@ -15,20 +14,54 @@ export class MoonPhase {
   animate(t) {
     requestAnimationFrame(this.animate.bind(this));
 
-    this.ctx.clearRect(0, 0, this.size, this.size);
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.width);
 
+    // draw full moon
     this.ctx.beginPath();
-    this.ctx.ellipse(this.size/2, this.size/2, this.size/4, this.size/2, 0, -Math.PI / 2, Math.PI / 2);
+    this.ctx.ellipse(
+      this.canvas.width / 2,
+      this.canvas.width / 2,
+      this.canvas.width / 2,
+      this.canvas.width / 2,
+      Math.PI / 4,
+      0,
+      2 * Math.PI
+    );
+    this.ctx.lineWidth = 10;
     this.ctx.stroke();
+    this.ctx.fillStyle = "#ff0";
+    this.ctx.fill();
 
+    // draw hidden half side
     this.ctx.beginPath();
-    this.ctx.ellipse(this.size/2, this.size/2, this.size/2, this.size/2, Math.PI / 4, 0, 2 * Math.PI);
-    this.ctx.stroke();
+    this.ctx.fillStyle = "black";
+    this.ctx.ellipse(
+      this.canvas.width / 2,
+      this.canvas.width / 2,
+      this.canvas.width / 2,
+      this.canvas.width / 2,
+      0,
+      -Math.PI / 2,
+      Math.PI / 2,
+      true
+    );
+    this.ctx.fill();
 
-
+    // draw hidden ellipse side
+    this.ctx.beginPath();
+    this.ctx.ellipse(
+      this.canvas.width / 2,
+      this.canvas.width / 2,
+      this.canvas.width * (60 / 180),
+      this.canvas.width / 2,
+      0,
+      -Math.PI / 2,
+      Math.PI / 2
+    );
+    this.ctx.fill();
   }
 }
 
 window.onload = () => {
-  new MoonPhase(500);
-}
+  new MoonPhase(1200);
+};
